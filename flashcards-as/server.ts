57a4cf18d8019df0220f2db286d9766ee2a16438
Isabel Motta <icavalcantemotta@gmail.com>
2024-05-22 19:07:48 -0300

    first commit

---
+++ flashcards-as/server.ts
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware para servir arquivos estáticos (como index.html)
+app.use(express.static('src'));
+
+/*app.get('/', (req, res) => {
+    res.send('hello world');
+});*/
+
+app.listen(PORT, '0.0.0.0',  () => {
+    console.log(`Servidor Node.js rodando na porta ${PORT}`);
+});
+
+// tsc C:path/server.ts
+
+// node server.ts
+// http://localhost:3000/
